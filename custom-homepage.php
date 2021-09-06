09338272db09bfdf421cb1faf35393402c0cbf3c
WillWebDev200 <william@webdev200.com>
2021-09-06 14:24:50 +0800

    update section page, colors and header

---
+++ custom-homepage.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php $headTitle = "Builder Homepage - Choose Category"; include('./components/layouts/Header.php'); ?>
+
+
+<body>
+
+
+   
+    <header class="header-custom">
+            <?php include('.//components/layouts/header-custom.php'); ?>
+    </header>
+
+
+    <main class="main-wrapper">
+
+    
+        <section class="side-tab">
+            <?php include('.//components/layouts/tab-section.php'); ?>
+        </section>
+
+        <section class="workspace">
+            test
+        </section>
+
+        
+        <section class="bottom-navigation">
+            <?php include('.//components/layouts/bottom-navigation.php'); ?>
+        </section>
+
+    </main>
+
+</body>
+
+
+
+
+
+<script src="../scripts/bootstrap.min.js"> </script>
+<?php include('./scripts/index-scripts.php'); ?>
+
+<?php include('./components/layouts/Footer.php'); ?>
+
+</html>
